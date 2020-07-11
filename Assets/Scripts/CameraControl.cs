c6aca7b5f7da74d97680bcdd31656ca4a98fd29b
eitrunix <52928831+eitrunix@users.noreply.github.com>
2020-07-11 03:09:23 -0400

    Player Movement Update

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+	public Transform target;
+	private Vector3 velocity = Vector3.zero;
+	public float smoothSpeed = 0.125f;
+	public Vector3 offset;
+
+
+	[SerializeField] CameraBounds2D bounds;
+	Vector2 maxXPositions, maxYPositions;
+
+	void Awake()
+	{
+		bounds.Initialize(GetComponent<Camera>());
+		maxXPositions = bounds.maxXlimit;
+		maxYPositions = bounds.maxYlimit;
+	}
+	void FixedUpdate()
+	{
+		Vector3 currentPosition = transform.position;
+		Vector3 targetPosition = new Vector3(Mathf.Clamp(target.position.x, maxXPositions.x, maxXPositions.y), Mathf.Clamp(target.position.y, maxYPositions.x, maxYPositions.y), currentPosition.z);
+		transform.position = Vector3.Lerp(currentPosition, targetPosition, Time.deltaTime * smoothSpeed);
+	}
+
+}
--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public SpriteRenderer bg;
 
+    private SpriteRenderer sprite;
     private Rigidbody2D rb2d;
     // Movespeed
     public float speed;
@@ -33,9 +35,14 @@ public class playerController : MonoBehaviour
     public float rememberGroundedFor;
     float lastTimeGrounded;
 
+    // Animations
+    private Animator animator;
+
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
         currentLives = defaultLives;
         currentHealth = defaultHealth;
     }
@@ -56,15 +63,32 @@ public class playerController : MonoBehaviour
 
     void Move()
     {
+        animator.SetBool("isRunning", true);
+
         float x = Input.GetAxisRaw("Horizontal");
         float moveBy = x * speed;
         rb2d.velocity = new Vector2(moveBy, rb2d.velocity.y);
+        
+        if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            animator.SetBool("isRunning", false);
+        }
+        else if(Input.GetAxisRaw("Horizontal") < 0)
+        {
+            sprite.flipX = true;
+        }       
+        else if(Input.GetAxisRaw("Horizontal") > 0)
+        {
+            sprite.flipX = false;
+        }
+
     }
 
     void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && (isGrounded || Time.time - lastTimeGrounded <= rememberGroundedFor || additionalJumps > 0))
         {
+            animator.SetBool("isJumping", true);
             rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
             additionalJumps--;
         }
@@ -75,6 +99,7 @@ public class playerController : MonoBehaviour
         Collider2D colliders = Physics2D.OverlapCircle(isGroundedChecker.position, checkGroundRadius, groundLayer);
         if (colliders != null)
         {
+            animator.SetBool("isJumping", false);
             isGrounded = true;
             additionalJumps = defaultAdditionalJumps;
         }
@@ -92,10 +117,12 @@ public class playerController : MonoBehaviour
     {
         if (rb2d.velocity.y < 0)
         {
+            animator.SetBool("isJumping", true);
             rb2d.velocity += Vector2.up * Physics2D.gravity * (fallMultiplier - 1) * Time.deltaTime;
         }
         else if (rb2d.velocity.y > 0 && !Input.GetKey(KeyCode.Space))
         {
+            animator.SetBool("isJumping", true);
             rb2d.velocity += Vector2.up * Physics2D.gravity * (lowJumpMultiplier - 1) * Time.deltaTime;
         }
     }
